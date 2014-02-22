ccb98b235ec02f24831187a8e5e94fa823c45d29
Joseph May <jwmay87@gmail.com>
2014-02-22 15:12:29 -0800

    add support for complete set of BibTeX field types

--- conbib/bibitem.py
+++ conbib/bibitem.py
@@ -7,35 +7,74 @@ parsing a LaTeX bib file including functions for matching and returning the
 contents of various bib item attributes. 
 
 """
-
 import re
 import sys
 import os.path
 
+
 __all__ = ['is_bibitem',
            'is_bibitem_end',
+           'is_item',
+           'is_address',
+           'is_annote',
            'is_author',
-           'is_title',
            'is_booktitle',
+           'is_chapter',
+           'is_crossref',
+           'is_edition',
+           'is_editor',
+           'is_eprint',
+           'is_howpublished',
+           'is_institution',
            'is_journal',
+           'is_key',
+           'is_month',
+           'is_note',
+           'is_number',
+           'is_organization',
+           'is_pages',
+           'is_publisher',
+           'is_school',
+           'is_series',
+           'is_title',
+           'is_type',
+           'is_url',
            'is_volume',
            'is_year',
-           'is_pages',
-           'is_item',
            'get_bibitem_type',
-           'get_author',
-           'get_title',
-           'get_booktitle',
-           'get_journal',
-           'get_volume',
-           'get_year',
-           'get_pages',
            'get_bibitems',
            'get_item_key',
            'get_attribute_item',
+           'get_address',
+           'get_annote',
+           'get_author',
+           'get_booktitle',
+           'get_chapter',
+           'get_crossref',
+           'get_edition',
+           'get_editor',
+           'get_eprint',
+           'get_howpublished',
+           'get_institution',
+           'get_journal',
+           'get_key',
+           'get_month',
+           'get_note',
+           'get_number',
+           'get_organization',
+           'get_pages',
+           'get_publisher',
+           'get_school',
+           'get_series',
+           'get_title',
+           'get_type',
+           'get_url',
+           'get_volume',
+           'get_year',
            'list_bibitems']
 
-# These are the allowe BibItem entry types, currently supported by BiBTeX.
+
+# the allowed BibTeX entry types
 ENTRY_TYPES = ['article',
                'book',
                'booklet',
@@ -67,12 +106,35 @@ class BibItem():
 
         """
         self.found = False
+
+        # BibTeX field types taken from http://en.wikipedia.org/wiki/BibTeX
+        self.address = None
+        self.annote = None
         self.author = None
-        self.title = None
+        self.booktitle = None
+        self.chapter = None
+        self.crossref = None
+        self.edition = None
+        self.editor = None
+        self.eprint = None
+        self.howpublished = None
+        self.institution = None
         self.journal = None
+        self.key = None
+        self.month = None
+        self.note = None
+        self.number = None
+        self.organization = None
+        self.pages = None
+        self.publisher = None
+        self.school = None
+        self.series = None
+        self.title = None
+        self.type = None
+        self.url = None
         self.volume = None
         self.year = None
-        self.pages = None
+
         self.missing = missing
 
         for line in inputfile:
@@ -80,26 +142,83 @@ class BibItem():
                 self.found = True
                 self.bibtype = get_bibitem_type(line)
             
+            elif is_address(line):
+                self.address = get_address(line)
+
+            elif is_annote(line):
+                self.annote = get_annote(line)
+            
             elif is_author(line):
                 self.author = get_author(line)
             
-            elif is_title(line):
-                self.title = get_title(line)
-
             elif is_booktitle(line):
-                self.title = get_booktitle(line)
+                self.booktitle = get_booktitle(line)
+
+            elif is_chapter(line):
+                self.chapter = get_chapter(line)
+
+            elif is_crossref(line):
+                self.crossref = get_crossref(line)
+
+            elif is_edition(line):
+                self.edition = get_edition(line)
+
+            elif is_editor(line):
+                self.editor = get_editor(line)
+
+            elif is_eprint(line):
+                self.eprint = get_eprint(line)
+
+            elif is_howpublished(line):
+                self.howpublished = get_howpublished(line)
+
+            elif is_institution(line):
+                self.institution = get_institution(line)
             
             elif is_journal(line):
                 self.journal = get_journal(line)
             
+            elif is_key(line):
+                self.key = get_key(line)
+
+            elif is_month(line):
+                self.month = get_month(line)
+
+            elif is_note(line):
+                self.note = get_note(line)
+
+            elif is_number(line):
+                self.number = get_number(line)
+
+            elif is_organization(line):
+                self.organization = get_organization(line)
+
+            elif is_pages(line):
+                self.pages = get_pages(line)
+
+            elif is_publisher(line):
+                self.publisher = get_publisher(line)
+
+            elif is_school(line):
+                self.school = get_school(line)
+
+            elif is_series(line):
+                self.series = get_series(line)
+
+            elif is_title(line):
+                self.title = get_title(line)
+
+            elif is_type(line):
+                self.type = get_type(line)
+
+            elif is_url(line):
+                self.url = get_url(line)
+
             elif is_volume(line):
                 self.volume = get_volume(line)
             
             elif is_year(line):
                 self.year = get_year(line)
-            
-            elif is_pages(line):
-                self.pages = get_pages(line)
 
             # If a bibitem is found and we encounter an attribute not parsed
             # above, then skip to the next line
@@ -230,14 +349,19 @@ class _MissingAttribute():
 # leaving all other information intact, you wouldn't need to create individual
 # BibItem objects, but rather, only parse the bib file for the title attribute.
 
+def is_address(line):
+    """Return True if line contains the address entry."""
+    return is_item('address', line)
+
+
+def is_annote(line):
+    """Return True if line contains the annote entry."""
+    return is_item('annote', line)
+
+
 def is_author(line):
     """Return True if line contains the author entry."""
     return is_item('author', line)
-
-
-def is_title(line):
-    """Return True if line contains the title entry."""
-    return is_item('title', line) 
 
 
 def is_booktitle(line):
@@ -245,9 +369,104 @@ def is_booktitle(line):
     return is_item('booktitle', line) 
 
 
+def is_chapter(line):
+    """Return True if line contains the chpater entry."""
+    return is_item('chapter', line) 
+
+
+def is_crossref(line):
+    """Return True if line contains the crossref entry."""
+    return is_item('crossref', line) 
+
+
+def is_edition(line):
+    """Return True if line contains the edition entry."""
+    return is_item('edition', line) 
+
+
+def is_editor(line):
+    """Return True if line contains the editor entry."""
+    return is_item('editor', line) 
+
+
+def is_eprint(line):
+    """Return True if line contains the eprint entry."""
+    return is_item('eprint', line) 
+
+
+def is_howpublished(line):
+    """Return True if line contains the howpublished entry."""
+    return is_item('howpublished', line) 
+
+
+def is_institution(line):
+    """Return True if line contains the institution entry."""
+    return is_item('institution', line) 
+
+
 def is_journal(line):
     """Return True if line contains the journal entry."""
     return is_item('journal', line) 
+
+
+def is_key(line):
+    """Return True if line contains the key entry."""
+    return is_item('key', line) 
+
+
+def is_month(line):
+    """Return True if line contains the month entry."""
+    return is_item('month', line) 
+
+
+def is_note(line):
+    """Return True if line contains the note entry."""
+    return is_item('note', line) 
+
+
+def is_number(line):
+    """Return True if line contains the number entry."""
+    return is_item('number', line) 
+
+
+def is_organization(line):
+    """Return True if line contains the organization entry."""
+    return is_item('organization', line) 
+
+
+def is_pages(line):
+    """Return True if line contains the pages entry."""
+    return is_item('pages', line) 
+
+
+def is_publisher(line):
+    """Return True if line contains the publisher entry."""
+    return is_item('publisher', line) 
+
+
+def is_school(line):
+    """Return True if line contains the school entry."""
+    return is_item('school', line) 
+
+
+def is_series(line):
+    """Return True if line contains the series entry."""
+    return is_item('series', line) 
+
+
+def is_title(line):
+    """Return True if line contains the title entry."""
+    return is_item('title', line) 
+
+
+def is_type(line):
+    """Return True if line contains the type entry."""
+    return is_item('type', line) 
+
+
+def is_url(line):
+    """Return True if line contains the url entry."""
+    return is_item('url', line) 
 
 
 def is_volume(line):
@@ -260,9 +479,14 @@ def is_year(line):
     return is_item('year', line) 
 
 
-def is_pages(line):
-    """Return True if line contains the pages entry."""
-    return is_item('pages', line) 
+def get_address(line):
+    """Return the contents of the `address' attribute."""
+    return get_attribute_item('address', 'contents', line)
+
+
+def get_annote(line):
+    """Return the contents of the `annote' attribute."""
+    return get_attribute_item('annote', 'contents', line)
 
 
 def get_author(line):
@@ -270,19 +494,109 @@ def get_author(line):
     return get_attribute_item('author', 'contents', line)
 
 
-def get_title(line):
-    """Return the contents of the `title' attribute."""
-    return get_attribute_item('title', 'contents', line)
-
-
 def get_booktitle(line):
     """Return the contents of the `booktitle' attribute."""
     return get_attribute_item('booktitle', 'contents', line)
 
 
+def get_chapter(line):
+    """Return the contents of the `chapter' attribute."""
+    return get_attribute_item('chapter', 'contents', line)
+
+
+def get_crossref(line):
+    """Return the contents of the `crossref' attribute."""
+    return get_attribute_item('crossref', 'contents', line)
+
+
+def get_edition(line):
+    """Return the contents of the `edition' attribute."""
+    return get_attribute_item('edition', 'contents', line)
+
+
+def get_editor(line):
+    """Return the contents of the `editor' attribute."""
+    return get_attribute_item('editor', 'contents', line)
+
+
+def get_eprint(line):
+    """Return the contents of the `eprint' attribute."""
+    return get_attribute_item('eprint', 'contents', line)
+
+
+def get_howpublished(line):
+    """Return the contents of the `howpublished' attribute."""
+    return get_attribute_item('howpublished', 'contents', line)
+
+
+def get_institution(line):
+    """Return the contents of the `institution' attribute."""
+    return get_attribute_item('institution', 'contents', line)
+
+
 def get_journal(line):
     """Return the contents of the `journal' attribute."""
     return get_attribute_item('journal', 'contents', line)
+
+
+def get_key(line):
+    """Return the contents of the `key' attribute."""
+    return get_attribute_item('key', 'contents', line)
+
+
+def get_month(line):
+    """Return the contents of the `month' attribute."""
+    return get_attribute_item('month', 'contents', line)
+
+
+def get_note(line):
+    """Return the contents of the `note' attribute."""
+    return get_attribute_item('note', 'contents', line)
+
+
+def get_number(line):
+    """Return the contents of the `number' attribute."""
+    return get_attribute_item('number', 'contents', line)
+
+
+def get_organization(line):
+    """Return the contents of the `organization' attribute."""
+    return get_attribute_item('organization', 'contents', line)
+
+
+def get_pages(line):
+    """Return the contents of the `pages' attribute."""
+    return get_attribute_item('pages', 'contents', line)
+
+
+def get_publisher(line):
+    """Return the contents of the `publisher' attribute."""
+    return get_attribute_item('publisher', 'contents', line)
+
+
+def get_school(line):
+    """Return the contents of the `school' attribute."""
+    return get_attribute_item('school', 'contents', line)
+
+
+def get_series(line):
+    """Return the contents of the `series' attribute."""
+    return get_attribute_item('series', 'contents', line)
+
+
+def get_title(line):
+    """Return the contents of the `title' attribute."""
+    return get_attribute_item('title', 'contents', line)
+
+
+def get_type(line):
+    """Return the contents of the `type' attribute."""
+    return get_attribute_item('type', 'contents', line)
+
+
+def get_url(line):
+    """Return the contents of the `url' attribute."""
+    return get_attribute_item('url', 'contents', line)
 
 
 def get_volume(line):
@@ -293,11 +607,6 @@ def get_volume(line):
 def get_year(line):
     """Return the contents of the `year' attribute."""
     return get_attribute_item('year', 'contents', line)
-
-
-def get_pages(line):
-    """Return the contents of the `pages' attribute."""
-    return get_attribute_item('pages', 'contents', line)
 
 
 def get_bibitem_type(line):
