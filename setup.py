b4d6d1e86b37d8f7fe21d28b122840e08c4027ac
Joseph May <jwmay87@gmail.com>
2014-07-31 12:44:47 -0700

    Clean up and add long descip

--- setup.py
+++ setup.py
@@ -3,13 +3,20 @@
 from distutils.core import setup
 from conbib import __version__ as version
 
-setup(name='convert-bibtex',
-      version=version,
-      description='Conversions and cite key generation for BibTeX files.',
-      packages=['conbib', 'conbib/tests'],
-      scripts=['bin/convert-bibtex'],
-      package_data={'conbib': ['tests/*']},
-      author='Joseph W. May',
-      author_email='jwmay@uw.edu',
-      url='http://www.gaussiantoolkit.org',
-     )
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='convert-bibtex',
+    version=version,
+    description='Conversions and cite key generation for BibTeX files.',
+    author='Joseph W. May (Li Research Group)',
+    author_email='jwmay@uw.edu',
+    url='http://www.gaussiantoolkit.org',
+    license = 'http://opensource.org/licenses/MIT',
+    long_description = long_description,
+    packages=['conbib', 'conbib/tests'],
+    scripts=['bin/convert-bibtex'],
+    package_data={'conbib': ['tests/*']},
+)
